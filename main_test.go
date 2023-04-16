705d576fae6eabe35157e4a4323a79b3a782ee1a
Christopher Basketfield <chris.basketfield@gmail.com>
2023-04-16 01:10:36 -0800

    TestGetProjectsAndDependencies

--- main_test.go
+++ main_test.go
@@ -1,47 +1,64 @@
 package main
 
 import (
+	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slices"
 )
 
 func TestGetProjectsAndDependencies(t *testing.T) {
-	assert.True(t, true)
+	absPath, err := filepath.Abs("./test_data")
+	if err != nil {
+		t.Error("Cannot find test data")
+	}
+	t.Setenv("DIR",absPath)
+	got_projects, got_deps := getProjectsAndDependencies()
+
+	expected_projects := []string{absPath+"/project1", absPath+"/project2"}
+	expected_deps := map[string][]string{
+		absPath+"/project1":[]string{"../modules/module1"},
+		absPath+"/project2":[]string{"../modules/module2"},
+	}
+
+	if !slices.Equal(got_projects, expected_projects) {
+		t.Errorf("Got projects:\n%q\nExpected projects:\n%q", got_projects, expected_projects)
+	}
+
+	for k,v := range expected_deps {
+		got_v, ok := got_deps[k]
+		if !ok {
+			t.Errorf("dependencies key expected but not found: %s\n",k)
+		}
+		if !slices.Equal(v, got_v) {
+			t.Errorf("dependencies[%s]\nExpected:%s\nGot:%s\n",k,v,got_v)
+		}
+	}
 }
 
 func TestReadAtlantisYaml(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestAddProjectsToConfig(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestFileExists(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestMakeProjectConfig(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestGetWhenModifiedPaths(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestCleanPaths(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestUnique(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestWriteAtlantisYaml(t *testing.T) {
-	assert.True(t, true)
 }
 
 func TestMain(t *testing.T) {
-	assert.True(t, true)
 }
